369914f20bb997c8dc2bf0653bee3b1cf46d3657
Shailesh <119912742+SShailesh-Dev@users.noreply.github.com>
2024-10-13 14:32:32 +0530

    Add files via upload

---
+++ Table Schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE netflix
+(
+show_id	VARCHAR(10),
+type VARCHAR(10),
+title VARCHAR(110),
+director VARCHAR(210),
+casts VARCHAR(1000),
+country	VARCHAR(150),
+date_added  VARCHAR(20),
+release_year INT,
+rating  VARCHAR(10),
+duration  VARCHAR(15),
+listed_in  VARCHAR(100),
+description  VARCHAR(300)
+);
